8384de59685042f0a66af02a6fb0511a23c22735
Aviral2403 <aviralkhandelwal03@gmail.com>
2025-03-18 23:12:40 +0530

    improved css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiX } from 'react-icons/fi';
-import '../assets/styles/Sidebar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiX } from "react-icons/fi";
+import "../assets/styles/Sidebar.css";
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <button className="sidebar-close" onClick={toggleSidebar}>
         <FiX />
       </button>
       <ul>
-        <li><Link to="/" onClick={toggleSidebar}>Home</Link></li>
-        <li><Link to="/add-user" onClick={toggleSidebar}>Add User</Link></li>
-        <li><Link to="/add-product" onClick={toggleSidebar}>Add Product</Link></li>
+        <li>
+          <Link to="/" onClick={toggleSidebar}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/add-user" onClick={toggleSidebar}>
+            Add User
+          </Link>
+        </li>
+        <li>
+          <Link to="/add-product" onClick={toggleSidebar}>
+            Add Product
+          </Link>
+        </li>
+        <li className="mobile-only">
+          <Link to="/users" onClick={toggleSidebar}>
+            Users
+          </Link>
+        </li>
+        <li className="mobile-only">
+          <Link to="/products" onClick={toggleSidebar}>
+            Products
+          </Link>
+        </li>
       </ul>
     </div>
   );
